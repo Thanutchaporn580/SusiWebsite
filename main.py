157488e637a668fbc78a6949b19dc7d7c5bbf54a
Thanutchaporn Boonyavong <tgfonefone2559@gmail.com>
2025-02-23 22:58:40 +0700

    Add route for faq page.

--- main.py
+++ main.py
@@ -255,5 +255,9 @@ def contact():
 def description():
     return render_template("description.html")
 
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
